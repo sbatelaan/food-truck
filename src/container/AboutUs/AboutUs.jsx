14a627ac59ab60bb6833138aaffabb77cc89b4b9
stephen Batelaan <sbatelaan91@gmail.com>
2023-07-08 17:56:02 -0700

    about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { images } from '../../constants';
 
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutUs app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutUs-overlay flex__center'>
+      <img src={images.G} alt='G' />
+    </div>
+
+    <div className='app__aboutUs-content flex__center'>
+      <div className='app__aboutUs-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt='spoon' className='spoon_img'/>
+        <p className='p__opensans'>Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutUs-content_knife flex__center'>
+        <img src={images.knife} alt="knife"/>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Header.css';
 
+
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavor" />
+      <h1 className='app__header-h1'>Your Header Here</h1>
+      <p className='p__opensans' style={{ margin: '2rem o' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+  <img src={images.welcome} alt='menu item' />
+    </div>
   </div>
 );
 
